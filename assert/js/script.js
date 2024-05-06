03d590795bc3fe701acb2abc50daa53aa359cee1
hiralvavadiya <thisishiralvavadiya@gmail.com>
2024-05-06 09:57:10 +0530

    final commit

---
+++ assert/js/script.js
@@ -0,0 +1,145 @@
+$(document).ready(function(){
+    $("#slider1").owlCarousel({
+
+        nav:true,
+        items:1,
+        loop:true,
+        margin:22,
+        // center:true,
+        // mouseDrag:true,
+        // touchDrag:true,
+        // pullDrag:true,
+        // freeDrag:true
+        dots:false,
+        navText:['<i class="fa-solid fa-angles-left"></i>','<i class="fa-solid fa-angles-right"></i>'],
+        // autoplay:true,
+        // autoplayTimeout:2000,
+        // autoplayHoverPause:true,
+        // animateOut: 'animate__fadeInUp',
+        // animateIn: 'animate__fadeOutDown',
+        // animateOut: 'animate__fadeInUp',
+        // smartSpeed:450
+    
+    });
+});
+
+
+$(document).ready(function(){
+    $("#slider2").owlCarousel({
+        nav:true,
+        items:8,
+        // loop:true,
+        // margin:20,
+        // center:true,
+        // mouseDrag:true,
+        // touchDrag:true,
+        // pullDrag:true,
+        // freeDrag:true
+        dots:false,
+        navText:['<','>'],
+        // autoplay:true,
+        // autoplayTimeout:2000,
+        // autoplayHoverPause:true,
+        // animateOut: 'animate__fadeInUp',
+        // animateIn: 'animate__fadeOutDown',
+        // animateOut: 'animate__fadeInUp',
+        // smartSpeed:450
+    
+    });
+});
+
+$(document).ready(function(){
+    $("#slider3").owlCarousel({
+        nav:true,
+        items:4,
+        // loop:true,
+        // margin:20,
+        // center:true,
+        // mouseDrag:true,
+        // touchDrag:true,
+        // pullDrag:true,
+        // freeDrag:true
+        dots:true,
+        navText:['<','>'],
+        // autoplay:true,
+        // autoplayTimeout:2000,
+        // autoplayHoverPause:true,
+        // animateOut: 'animate__fadeInUp',
+        // animateIn: 'animate__fadeOutDown',
+        // animateOut: 'animate__fadeInUp',
+        // smartSpeed:450
+    
+    });
+});
+
+$(document).ready(function(){
+    $("#slider4").owlCarousel({
+        nav:true,
+        items:4,
+        // loop:true,
+        // margin:20,
+        // center:true,
+        // mouseDrag:true,
+        // touchDrag:true,
+        // pullDrag:true,
+        // freeDrag:true
+        dots:true,
+        navText:['<','>'],
+        // autoplay:true,
+        // autoplayTimeout:2000,
+        // autoplayHoverPause:true,
+        // animateOut: 'animate__fadeInUp',
+        // animateIn: 'animate__fadeOutDown',
+        // animateOut: 'animate__fadeInUp',
+        // smartSpeed:450
+    
+    });
+});
+
+$(document).ready(function(){
+    $("#slider5").owlCarousel({
+        nav:true,
+        items:2,
+        // loop:true,
+        // margin:20,
+        // center:true,
+        // mouseDrag:true,
+        // touchDrag:true,
+        // pullDrag:true,
+        // freeDrag:true
+        dots:false,
+        navText:['<','>'],
+        // autoplay:true,
+        // autoplayTimeout:2000,
+        // autoplayHoverPause:true,
+        // animateOut: 'animate__fadeInUp',
+        // animateIn: 'animate__fadeOutDown',
+        // animateOut: 'animate__fadeInUp',
+        // smartSpeed:450
+    
+    });
+});
+
+$(document).ready(function(){
+    $("#slider6").owlCarousel({
+        // nav:true,
+        items:5,
+        // loop:true,
+        // margin:20,
+        // center:true,
+        // mouseDrag:true,
+        // touchDrag:true,
+        // pullDrag:true,
+        // freeDrag:true
+        dots:true,
+        // navText:['<','>'],
+        // autoplay:true,
+        // autoplayTimeout:2000,
+        // autoplayHoverPause:true,
+        // animateOut: 'animate__fadeInUp',
+        // animateIn: 'animate__fadeOutDown',
+        // animateOut: 'animate__fadeInUp',
+        // smartSpeed:450
+    
+    });
+});
